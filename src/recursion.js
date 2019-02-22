c55b86024cf55e7be279aadbdd9eaa896a733c80
chrisamurao <freshpotatoes@gmail.com>
2019-02-21 20:59:26 -0800

    completed some, skipped some

--- src/recursion.js
+++ src/recursion.js
@@ -156,48 +156,99 @@ var multiply = function(x, y) {
 
 // 13. Write a function that divides two numbers without using the / operator or
 // Math methods to arrive at an approximate quotient (ignore decimal endings).
-var divide = function(x, y) {};
+var divide = function(x, y) {
+  if (y === 0) return NaN;
+  if (y === 1) return x;
+
+  if (x < 0 && y < 0) return divide(-x, -y);
+  if ((x < 0) ^ (y < 0)) return -divide(-x, y);
+  if (x > y) return 1 + divide(x - y, y);
+  return 0;
+};
 
 // 14. Find the greatest common divisor (gcd) of two positive numbers. The GCD of two
 // integers is the greatest integer that divides both x and y with no remainder.
 // gcd(4,36); // 4
 // http://www.cse.wustl.edu/~kjg/cse131/Notes/Recursion/recursion.html
 // https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
-var gcd = function(x, y) {};
+var gcd = function(x, y) {
+  if (x < 0) return null;
+  if (y === 0) return x;
+  return gcd(y, modulo(x, y));
+};
 
 // 15. Write a function that compares each character of two strings and returns true if
 // both are identical.
 // compareStr('house', 'houses') // false
 // compareStr('tomato', 'tomato') // true
-var compareStr = function(str1, str2) {};
+var compareStr = function(str1, str2) {
+  if (str1.length === 0 && str2.length === 0) return true;
+  if (str1[0] !== str2[0]) return false;
+  return compareStr(str1.slice(1, str1.length), str2.slice(1, str2.length));
+};
 
 // 16. Write a function that accepts a string and creates an array where each letter
 // occupies an index of the array.
-var createArray = function(str) {};
+var createArray = function(str) {
+  if (str.length === 0) return [];
+  return [str[0]].concat(createArray(str.slice(1, str.length)));
+};
 
 // 17. Reverse the order of an array
-var reverseArr = function(array) {};
+var reverseArr = function(A) {
+  if (A.length === 0) return [];
+  return [A[A.length - 1]].concat(reverseArr(A.slice(0, A.length - 1)));
+};
 
 // 18. Create a new array with a given value and length.
 // buildList(0,5) // [0,0,0,0,0]
 // buildList(7,3) // [7,7,7]
-var buildList = function(value, length) {};
+var buildList = function(value, length) {
+  if (length === 0) return [];
+  return [value].concat(buildList(value, length - 1));
+};
 
 // 19. Implement FizzBuzz. Given integer n, return an array of the string representations of 1 to n.
 // For multiples of three, output 'Fizz' instead of the number.
 // For multiples of five, output 'Buzz' instead of the number.
 // For numbers which are multiples of both three and five, output “FizzBuzz” instead of the number.
 // fizzBuzz(5) // ['1','2','Fizz','4','Buzz']
-var fizzBuzz = function(n) {};
+
+// NOTE: I've had difficulty doing this problem with only one input "n"
+// With another input to match on, this problem would be trivial
+// As of now, it works, but only in reverse, so it would need a helper function
+// The problem lies in determining what the current number is with only the information n
+// 2/21/19
+
+var fizzBuzz = function(n) {
+  if (n === 1) return [1];
+  if (n % 3 === 0 && n % 5 === 0) return ["FizzBuzz"].concat(fizzBuzz(n - 1));
+  if (n % 3 === 0) return ["Fizz"].concat(fizzBuzz(n - 1));
+  if (n % 5 === 0) return ["Buzz"].concat(fizzBuzz(n - 1));
+
+  return [n + ""].concat(fizzBuzz(n - 1));
+};
 
 // 20. Count the occurence of a value in a list.
 // countOccurrence([2,7,4,4,1,4], 4) // 3
 // countOccurrence([2,'banana',4,4,1,'banana'], 'banana') // 2
-var countOccurrence = function(array, value) {};
+var countOccurrence = function(array, value) {
+  if (array.length === 0) return 0;
+  if (array[0] === value) {
+    return 1 + countOccurrence(array.slice(1, array.length), value);
+  } else {
+    return countOccurrence(array.slice(1, array.length), value);
+  }
+};
 
 // 21. Write a recursive version of map.
 // rMap([1,2,3], timesTwo); // [2,4,6]
-var rMap = function(array, callback) {};
+var rMap = function(array, callback) {
+  if (array.length === 0) return [];
+  return [callback(array[0])].concat(
+    rMap(array.slice(1, array.length), callback)
+  );
+};
 
 // 22. Write a function that counts the number of times a key occurs in an object.
 // var obj = {'e':{'x':'y'},'t':{'r':{'e':'r'},'p':{'y':'r'}},'y':'e'};
@@ -227,16 +278,35 @@ var fibonacci = function(n) {};
 // nthFibo(5); // 5
 // nthFibo(7); // 13
 // nthFibo(3); // 2
-var nthFibo = function(n) {};
+var nthFibo = function(n) {
+  if (n < 0) return null;
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  return nthFibo(n - 1) + nthFibo(n - 2);
+};
 
 // 27. Given an array of words, return a new array containing each word capitalized.
 // var words = ['i', 'am', 'learning', 'recursion'];
 // capitalizedWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
-var capitalizeWords = function(array) {};
+var capitalizeWords = function(array) {
+  if (array.length === 0) return [];
+  return [array[0].toUpperCase()].concat(
+    capitalizeWords(array.slice(1, array.length))
+  );
+};
 
 // 28. Given an array of strings, capitalize the first letter of each index.
 // capitalizeFirst(['car','poop','banana']); // ['Car','Poop','Banana']
-var capitalizeFirst = function(array) {};
+var capitalizeFirst = function(array) {
+  const capitalizer = str =>
+    str.charAt(0).toUpperCase() + str.slice(1, str.length);
+
+  if (array.length === 0) return [];
+  return [capitalizer(array[0])].concat(
+    capitalizeFirst(array.slice(1, array.length))
+  );
+};
 
 // 29. Return the sum of all even numbers in an object containing nested objects.
 // var obj1 = {
@@ -247,43 +317,131 @@ var capitalizeFirst = function(array) {};
 //   e: {e: {e: 2}, ee: 'car'}
 // };
 // nestedEvenSum(obj1); // 10
-var nestedEvenSum = function(obj) {};
+
+// Would it possible to accomplish this without a counter variable?
+var nestedEvenSum = function(obj) {
+  let sum = 0;
+  for (let key in obj) {
+    if (typeof obj[key] === "object") sum += nestedEvenSum(obj[key]);
+    if (typeof obj[key] === "number" && obj[key] % 2 === 0) sum += obj[key];
+  }
+  return sum;
+};
 
 // 30. Flatten an array containing nested arrays.
 // flatten([1,[2],[3,[[4]]],5]); // [1,2,3,4,5]
-var flatten = function(array) {};
+var flatten = function(array) {
+  if (array.length === 0) return [];
+  if (Array.isArray(array[0])) {
+    return flatten(array[0]).concat(flatten(array.slice(1, array.length)));
+  } else {
+    return [array[0]].concat(flatten(array.slice(1, array.length)));
+  }
+};
 
 // 31. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {p:1, o:2, t:2, a:1}
-var letterTally = function(str, obj) {};
+var letterTally = function(str, obj = {}) {
+  if (str.length === 0) return obj;
+
+  obj[str[0]] ? obj[str[0]]++ : (obj[str[0]] = 1);
+
+  return letterTally(str.slice(1, str.length), obj);
+};
 
 // 32. Eliminate consecutive duplicates in a list. If the list contains repeated
 // elements they should be replaced with a single copy of the element. The order of the
 // elements should not be changed.
 // compress([1,2,2,3,4,4,5,5,5]) // [1,2,3,4,5]
 // compress([1,2,2,3,4,4,2,5,5,5,4,4]) // [1,2,3,4,2,5,4]
-var compress = function(list) {};
+var compress = function(list) {
+  if (list.length === 0) return [];
+
+  // if two zeroes, skip
+  if (list[0] === list[1]) return compress(list.slice(1, list.length));
+
+  // else save
+  return [list[0]].concat(compress(list.slice(1, list.length)));
+};
 
 // 33. Augment every element in a list with a new value where each element is an array
 // itself.
 // augmentElements([[],[3],[7]], 5); // [[5],[3,5],[7,5]]
-var augmentElements = function(array, aug) {};
+var augmentElements = function(array, aug) {
+  if (array.length === 0) return [];
+  array[0].push(aug);
+  // note: remember that the push function's return value is the length of the array
+  // we don't want to push there because it will push the length
+  //             |
+  //             v
+  return [array[0]].concat(augmentElements(array.slice(1, array.length), aug));
+};
 
 // 34. Reduce a series of zeroes to a single 0.
 // minimizeZeroes([2,0,0,0,1,4]) // [2,0,1,4]
 // minimizeZeroes([2,0,0,0,1,0,0,4]) // [2,0,1,0,4]
-var minimizeZeroes = function(array) {};
+var minimizeZeroes = function(array) {
+  if (array.length === 0) return [];
+
+  // if two zeroes, skip
+  if (array[0] === 0 && array[1] === 0)
+    return minimizeZeroes(array.slice(1, array.length));
+
+  // else save
+  return [array[0]].concat(minimizeZeroes(array.slice(1, array.length)));
+};
 
 // 35. Alternate the numbers in an array between positive and negative regardless of
 // their original sign. The first number in the index always needs to be positive.
 // alternateSign([2,7,8,3,1,4]) // [2,-7,8,-3,1,-4]
 // alternateSign([-2,-7,8,3,-1,4]) // [2,-7,8,-3,1,-4]
-var alternateSign = function(array) {};
+var alternateSign = function(array) {
+  if (array.length === 0) return [];
+  if (array[0] < 0) array[0] = -array[0];
+  if (array[1] > 0) array[1] = -array[1];
+  return [array[0], array[1]].concat(alternateSign(array.slice(2)));
+};
 
 // 36. Given a string, return a string with digits converted to their word equivalent.
 // Assume all numbers are single digits (less than 10).
 // numToText("I have 5 dogs and 6 ponies"); // "I have five dogs and six ponies"
-var numToText = function(str) {};
+var numToText = function(str) {
+  if (str.length === 0) return "";
+
+  let add = str[0];
+
+  switch (str[0]) {
+    case "1":
+      add = "one";
+      break;
+    case "2":
+      add = "two";
+      break;
+    case "3":
+      add = "three";
+      break;
+    case "4":
+      add = "four";
+      break;
+    case "5":
+      add = "five";
+      break;
+    case "6":
+      add = "six";
+      break;
+    case "7":
+      add = "seven";
+      break;
+    case "8":
+      add = "eight";
+      break;
+    case "9":
+      add = "nine";
+      break;
+  }
+
+  return add + numToText(str.slice(1, str.length));
+};
 
 // *** EXTRA CREDIT ***
 
